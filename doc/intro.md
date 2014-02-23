a49b168ea19843fa7073097ef7e3f72dffcf548f
Robbie Clutton <robert.clutton@gmail.com>
2014-02-23 09:20:10 -0500

    Init

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to robb1e
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/robb1e/core_test.clj
@@ -0,0 +1,7 @@
+(ns robb1e.core-test
+  (:require [clojure.test :refer :all]
+            [robb1e.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
