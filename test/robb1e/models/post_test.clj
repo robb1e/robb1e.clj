e28d20cf457f6d44989d3c5bdf441d9f89ab46f0
Robbie Clutton <robert.clutton@gmail.com>
2014-03-23 11:06:28 -0400

    Use clj-time explicitly

--- test/robb1e/models/post_test.clj
+++ test/robb1e/models/post_test.clj
@@ -1,9 +1,8 @@
 (ns robb1e.models.post-test
   (:require [clojure.test :refer :all]
-            [robb1e.models.post :as post]))
-
-(use 'clj-time.core)
-(use 'clj-time.coerce)
+            [robb1e.models.post :as post]
+            [clj-time.core :as tm]
+            [clj-time.coerce :as coerce]))
 
 (def db "postgresql://localhost:5432/robb1e_test")
 
@@ -16,10 +15,10 @@
 (deftest can-insert-new-post
   (testing "Inserting new records, and retriving those"
     (is (= 0 (count (post/all db))))
-    (post/insert db {:excerpt "excerpt" :publication "publication" :uri "uri" :title "Hello, world" :published_at (to-sql-date (now))})
+    (post/insert db {:excerpt "excerpt" :publication "publication" :uri "uri" :title "Hello, world" :published_at (coerce/to-sql-date (tm/now))})
     (is (= 1 (count (post/all db))))))
 
 (deftest can-get-posts
   (testing "Retriving existing records"
-    (post/insert db {:excerpt "excerpt" :publication "publication" :uri "uri" :title "Hello, world" :published_at (to-sql-date (now))})
+    (post/insert db {:excerpt "excerpt" :publication "publication" :uri "uri" :title "Hello, world" :published_at (coerce/to-sql-date (tm/now))})
     (is (= "Hello, world" ((first (post/all db)) :title)))))
