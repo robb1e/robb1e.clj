a1a5c1b2878904a2c4032145f999f1026390aea7
Robbie Clutton <robert.clutton@gmail.com>
2014-04-03 08:38:12 -0400

    Use generated assets

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/data.json "0.2.4"]
                  [ring/ring-jetty-adapter "1.2.1"]
                  [hiccup "1.0.4"]
                  [compojure "1.1.6"]
--- src/robb1e/views/layout.clj
+++ src/robb1e/views/layout.clj
@@ -1,11 +1,18 @@
 (ns robb1e.views.layout
-  (:require [hiccup.page :as h]))
+  (:require [hiccup.page :as h]
+            [clojure.data.json :as json]))
+
+(def assets
+  ((json/read-str (slurp "resources/public/stylesheets/manifest.json")) "assets"))
+
+(defn asset [file]
+  (str "/stylesheets/" (assets file)))
 
 (defn common [body]
   (h/html5
    [:head
     [:title "robb1e dot com"]
-    (h/include-css "/stylesheets/application.css")]
+    (h/include-css (asset "application.css"))]
    [:body
     [:div {:id "header"}
      [:h1 {:class "container"} "robb1e dot com"]]
